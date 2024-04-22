b7f80c83431bc30674168f0dc5a76a80f7aa9679
ferhatergun <eferhat718@gmail.com>
2024-04-22 11:15:21 +0300

    satır indexi bilinen öğrencinin yoklama işlemleri yapıldı

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -4,32 +4,22 @@ const toLowerCase = require("../utils/toLowerCase");
 
 const userAttendance= async (req, res) => {
     // Yoklama İşlemini gerçekleştiren fonksiyon
-    const { studentNumber } = req.body; // Öğrenci numarasını al
+    const { studentIndex } = req.body; // Öğrenci numarasını al
     const spreadsheetId = process.env.SPREAD_SHEET_ID  // Google Sheets belge kimliği
 
     try {
         const { client, googleSheets } = await googleSheetsAuth(); // Yetkilendirme al
         const { getRows } = await getSheetsRow() // Tablodaki tüm verileri al
 
-
         // Öğrenci numarasının bulunduğu sütunu bul
         const studentColumnIndex = getRows.data.values[0].indexOf("öğrenci numarası");
         // studentColumnIndex = 0 dır çünkü öğrenci numarası ilk sütunda yer alır
-        console.log("satır 29", getRows.data.values[3][4]);
 
-        // Her öğrenci için öğrenci numarasını kontrol et ve en son etkinlik sütununa 1 yaz
-        for (let i = 1; i < getRows.data.values.length; i++) {
-            // i=1 den başlıyor çünkü 0. satır başlık satırıdır
-            // kaç tane öğrenci varsa o kadar döngü dönecek
-            // tüm öğrenciler için öğrenci numarasını kontrol eder
-            if (getRows.data.values[i][studentColumnIndex] == studentNumber)
-            //getRows.data.values[i][0] yani öğrenci numarası ile girilen öğrenci numarasını karşılaştırıyoruz
-            {
-                const lastColumnIndex = getRows.data.values[0].length; // Son sütunun indeksini al
-                getRows.data.values[i][lastColumnIndex - 1] = 1; // En sondaki etkinlik sütununa 1 yaz
-                break; // Öğrenci bulunduğunda döngüden çık
-            }
-        }
+        const lastColumnIndex = getRows.data.values[0].length -1; // Son sütunun indeksini al
+        // studentIndex excel de 5 ise matris te 4 numaradır diziler 0 dan başladığı için
+        getRows.data.values[studentIndex-1][lastColumnIndex] = 1; // En sondaki etkinlik sütununa 1 yaz
+
+
         // Veriyi güncelle
         googleSheets.spreadsheets.values.update({
             auth: client,
@@ -41,7 +31,7 @@ const userAttendance= async (req, res) => {
             },
         });
 
-        res.json({ success: true, message: "Yoklama başarıyla güncellendi." });
+        res.status(200).json({ success: true, message: "Yoklama başarıyla güncellendi." });
     } catch (error) {
         console.error("Hata:", error);
         res.status(500).json({ success: false, message: "Yoklama güncellenirken bir hata oluştu." });
@@ -54,13 +44,12 @@ const getUserCheck = async (req, res) => {
     const spreadsheetId = process.env.SPREAD_SHEET_ID 
     const { client, googleSheets } = await googleSheetsAuth();
     const { getRows } = await getSheetsRow()
-    console.log("name", name);
     
     // İsim sütununun indeksini bul
     const nameColumnIndex = getRows.data.values[0].indexOf("İsim"); // İsim sütununun indeksini aldı // 1 oldu
     console.log("nameColumnIndex", nameColumnIndex);
 
-    const matchingStudents = [];
+    const matchingStudents = []; // eşleşen isimleri biriktirmek için boş bir dizi oluştur
     for (let i = 1; i < getRows.data.values.length; i++) {
       if (toLowerCase(getRows.data.values[i][nameColumnIndex]) === name) {
         matchingStudents.push({
@@ -69,8 +58,7 @@ const getUserCheck = async (req, res) => {
         });
       }
     }
-    console.log("matchingStudents", matchingStudents); 
-    // return matchingStudents;
+    res.status(200).json({ success: true, data: matchingStudents });
 
 }
 
@@ -78,3 +66,19 @@ module.exports = {
     userAttendance,
     getUserCheck
 };
+
+
+// matriste öğrenci numarası ile işleşen satırdaki son sütuna 1 yazdıran kod yedekte kalsın
+        // Her öğrenci için öğrenci numarasını kontrol et ve en son etkinlik sütununa 1 yaz
+/*         for (let i = 1; i < getRows.data.values.length; i++) {
+            // i=1 den başlıyor çünkü 0. satır başlık satırıdır
+            // kaç tane öğrenci varsa o kadar döngü dönecek
+            // tüm öğrenciler için öğrenci numarasını kontrol eder
+            if (getRows.data.values[i][studentColumnIndex] == studentNumber)
+            //getRows.data.values[i][0] yani öğrenci numarası ile girilen öğrenci numarasını karşılaştırıyoruz
+            {
+                const lastColumnIndex = getRows.data.values[0].length; // Son sütunun indeksini al
+                getRows.data.values[i][lastColumnIndex - 1] = 1; // En sondaki etkinlik sütununa 1 yaz
+                break; // Öğrenci bulunduğunda döngüden çık
+            }
+        } */
